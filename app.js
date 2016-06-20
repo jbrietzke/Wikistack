7fca90dd2038d3f58d93f1aa190d07dd0e3dc5d8
Jackson Brietzke <jack.brietzk@gmail.com>
2016-06-20 12:05:58 -0400

    Finished syncing

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var swig = require('swig');
+var models = require('./models');
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+app.set('views', __dirname + '/views');
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files
+// have it use swig to do so
+app.engine('html', swig.renderFile);
+// turn of swig's caching
+swig.setDefaults({cache: false});
+
+
+app.use(function(req,res,next){
+  console.log(req);
+  next();
+});
+
+app.use(express.static('public'));
+
+app.get('/', function(req,res, next){
+  res.render('index', {});
+});
+
+models.User.sync({})
+.then(function () {
+    return models.Page.sync({})
+})
+.then(function () {
+    app.listen(3001, function () {
+        console.log('Server is listening on port 3001!');
+    });
+})
+.catch(console.error);
+
