8b2516b391421a62c77e08afc46498e86517aed6
Jackson Brietzke <jack.brietzk@gmail.com>
2016-06-20 15:19:38 -0400

    Created Hook

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,13 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+var Page = models.Page;
+var User = models.User;
 
+router.post('/', function(req, res, next) {
+
+  // STUDENT ASSIGNMENT:
+  // add definitions for `title` and `content`
+
+
+  var page = Page.build({
+    title: req.body.title,
+    content: req.body.content
+  }, {
+    hooks: {
+      generateUrlTitle : function (title) {
+        if (title) {
+    // Removes all non-alphanumeric characters from title
+    // And make whitespace underscore
+          page.urlTitle = title.replace(/\s+/g, '_').replace(/\W/g, '');
+        } else {
+    // Generates random 5 letter string
+          page.urlTitle = Math.random().toString(36).substring(2, 7);
+        }
+      }
+    }
+  });
+
+  // STUDENT ASSIGNMENT:
+  // make sure we only redirect *after* our save is complete!
+  // note: `.save` returns a promise or it can take a callback.
+  page.save().then(function(){
+    res.redirect('/')
+  }).catch(err);
+  // -> after save -> res.redirect('/');
+});
 
 router.get('/', function(req, res, next) {
   res.redirect('/');
 });
 
 router.post('/', function(req, res, next) {
-  res.send('got to POST /wiki/');
+  res.json(req.body);
 });
 
 router.get('/add', function(req, res, next) {
